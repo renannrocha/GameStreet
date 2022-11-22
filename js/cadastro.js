d13c3abbd45a6d0daa4e1f1b37ce74bc3e0c4dbb
GercyanoAlves <gercyanoalves@gmail.com>
2022-11-21 22:20:46 -0300

    New login e cadastrar, validar login e senha

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,4 @@
-let form = document.getElementById("register-form");
+/*let form = document.getElementById("form-group");
 let subimt = document.getElementById("btn mt-4");
 
 // Evento que dispara as validações
@@ -6,4 +6,19 @@ subimt.addEventListener('click', function(e){
 
     e.preventDefault;
     console.log('Funcionou');
-});
+});*/
+
+//Serve para logar, é uma validação com senha, 
+//Pega os campos digitados, compara e se for verdadeiro 
+// é login com sucesso, se tiver um falso, mostra invalido
+function logar(){
+
+    var login = document.getElementById('email').value;
+    var senha = document.getElementById('password').value;
+
+    if(login == "admin@admin.com" && senha == "123456"){
+        alert('Login com sucesso');
+    }else{
+        alert('Usuario ou senha invalido');
+    }
+}	
