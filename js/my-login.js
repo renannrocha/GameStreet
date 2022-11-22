ec510a84cb8cd10772e50a0b4edf27cab246b547
renannrocha <renanrocha8897@gmail.com>
2022-11-22 12:01:27 -0300

    0.1

--- js/my-login.js
+++ js/my-login.js
@@ -59,8 +59,8 @@ $(function() {
 	$(".my-login-validation").submit(function() {
 		var form = $(this);
         if (form[0].checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+			event.preventDefault();
+			event.stopPropagation();
         }
 		form.addClass('was-validated');
 	});
