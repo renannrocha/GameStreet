8165de0d757bc61c76104d169674132c03c56f56
gomesleonardo <gomesleonardo0109@gmail.com>
2022-11-22 08:43:06 -0300

    validacao utilizando jquery

---
+++ js/validacaocad.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#formCad").validate({
+        rules:{
+            nome:{
+                required: true,
+                minlength:3
+            },
+            email:{
+                email: true,
+                required: true
+               
+            },
+            senha:{
+                minlength:3,
+                maxlength:10
+            },
+            confSenha:{
+                equalTo:'#senha'
+            }
+        }
+    })
+});
