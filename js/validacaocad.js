4fd26eb3d7aef206bdd89a7d84a6d3187a04b2f7
renannrocha <renanrocha8897@gmail.com>
2022-11-22 11:54:10 -0300

    1.0

--- js/validacaocad.js
+++ js/validacaocad.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
             email:{
                 email: true,
                 required: true
-               
             },
             senha:{
                 minlength:3,
